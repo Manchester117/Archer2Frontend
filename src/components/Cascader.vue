681d5dec2883e1b3053fb0714632c9b7df58e6d8
Peng.Zhao <654882654@163.com>
2018-12-17 17:20:36 +0800

    增加三级联动

--- src/components/Cascader.vue
+++ src/components/Cascader.vue
@@ -1,35 +1,46 @@
 <template>
   <v-layout class="px-2 py-2">
     <v-select
-      v-model="UnitList.select"
-      :hint="`${UnitList.select.name}, ${UnitList.select.id}`"
-      :items="UnitList.items"
+      v-model="projectSelected"
+      :hint="`${projectData.select.name}, ${projectData.select.id}`"
+      :items="projectData.items"
       item-text="name"
       item-value="id"
       label="项目"
       persistent-hint
       return-object
-      hide-details=true
+      hide-details
       class="px-1"
       dense
+      small-chips
     ></v-select>
     <v-select
-      v-model="DepartmentList.select"
-      :hint="`${DepartmentList.select.name}, ${DepartmentList.select.id}`"
-      :items="DepartmentList.items"
+      v-model="serviceSelected"
+      :hint="`${serviceData.select.name}, ${serviceData.select.id}`"
+      :items="serviceData.items"
       item-text="name"
       item-value="id"
       label="服务"
       persistent-hint
       return-object
-      hide-details="true"
+      hide-details
       class="px-1"
       dense
+      small-chips
     ></v-select>
     <v-select
-      :items="UserList"
+      v-model="apiSelected"
+      :hint="`${apiData.select.name}, ${apiData.select.id}`"
+      :items="apiData.items"
+      item-text="name"
+      item-value="id"
       label="接口"
+      persistent-hint
+      return-object
+      hide-details
       class="px-1"
+      dense
+      small-chips
     ></v-select>
   </v-layout>
 </template>
@@ -38,68 +49,108 @@
   export default {
     data () {
       return {
-        UnitSelected: "",
-        UnitList: [],
-        DepartmentSelected: "",
-        DepartmentList: [],
-        UserSelected: "",
-        UserList: []
+        projectSelected: {},      // 被选中的项目
+        projectData: {},          // 当前项目列表
+        originProjectData: {},    // 全部项目列表
+        
+        serviceSelected: {},      // 被选中的服务
+        serviceData: {},          // 当前服务列表
+        originServiceData: {},    // 全部服务列表
+
+        apiSelected: {},          // 被选中的接口
+        apiData: {},              // 当前接口列表
+        originApiData: {},        // 全部接口列表
       }
     },
     watch: {
-      UnitSelected: function (val) {
-          this.DepartmentList = this.DepartmentList.filter(function (element) { return element.pid == val });
-          this.DepartmentSelected = this.DepartmentList.length > 0 ? this.DepartmentList[0].id : "--";
+      // 监控项目下拉框
+      projectSelected: function (val) {
+        // 使用当前选择到的项目id,对服务列表进行过滤,拿到该项目下的所有服务
+        this.serviceData.items = this.serviceOriginData.items.filter(function (item) { return item.pid == val.id })
+        // 将当前的服务列表的值置为第一个服务(空的-默认当前项目下的所有服务)
+        this.serviceSelected = this.serviceData.items[0]
       },
-      DepartmentSelected: function (val) {
-          this.UserList = this.UserList.filter(function (element){ return element.pid == val});
-          this.UserSelected = this.UserList.length > 0 ? this.UserList[0].id : "--";
+      serviceSelected: function (val) {
+        // 使用当前选择到的服务id,对接口列表进行过滤,拿到该服务下的所有接口
+        this.apiData.items = this.apiOriginData.items.filter(function (item){ return item.pid == val.id })
+        // 将当前的接口列表的值职位第一个接口(空的-默认当前服务下的所有接口)
+        this.apiSelected = this.apiData.items[0]
       }
     },
-    mounted() {
-      let unitList = this.getProjectData().UnitList
-      let departmentList = this.getProjectData().DepartmentList
-      let userList = this.getProjectData().userList
-      
-      this.UnitList = unitList
-      this.UnitSelected = unitList.length > 0 ? UnitList[0].id : "--";
+    created() {
+      this.getProjectData()
+      this.getServiceData()
+      this.getApiData()
 
-      let unitId = this.UnitSelected;
-      this.DepartmentList = departmentList.filter(function (element) { return element.pid == unitId });
-      this.DepartmentSelected = departmentList.length > 0 ? departmentList[0].id : "--";
-
-      let departId = this.DepartmentSelected;
-      this.UserList = userList.filter(function (element){ return element.pid == departId});
-      this.UserSelected = userList.length > 0 ? userList[0].id : "--";
+      this.initMenu()
     },
     methods: {
-      getProjectData() {
-        this.UnitList = {
-          select: { name: "--", id:"--" },
+      // 获取项目名称-ID
+      getProjectData: function() {
+        this.projectData = {
+          select: { name: "---", id:"0" },
           items: [
-            { name: "小程序", id: "1" },
-            { name: "B端项目", id: "2" },
+            { name: "---", id:"0" },
+            { name: "项目-1", id: "1" },
+            { name: "项目-2", id: "2" },
           ]
         },
-        this.DepartmentList = {
-          select: { name: "--", id: "--", pid: '--' },
+        // 借助JSON进行深度复制,保留全部数据
+        this.projectOriginData = JSON.parse(JSON.stringify(this.projectData))
+      },
+      // 获取服务名称-ID-关联项目ID
+      getServiceData: function() {
+        this.serviceData = {
+          select: { name: "---", id: "0", pid: '0' },
           items: [
-            { name:"服务1", id:"1", pid:"1" },
-            { name:"服务2", id:"2", pid:"1" },
-            { name:"服务3", id:"3", pid:"2" },
-            { name:"服务4", id:"4", pid:"2" }
+            { name: "---", id: "0", pid: '0' },
+            { name: "项目1-服务1", id: "1", pid: "1" },
+            { name: "项目1-服务2", id: "2", pid: "1" },
+            { name: "项目2-服务1", id: "3", pid: "2" },
+            { name: "项目2-服务2", id: "4", pid: "2" }
           ]
         },
-        this.UserList = [
-          {name:"111", id:"1", pid:"1"},
-          {name:"112", id:"2", pid:"1"},
-          {name:"121", id:"3", pid:"2"},
-          {name:"122", id:"4", pid:"2"},
-          {name:"211", id:"5", pid:"3"},
-          {name:"212", id:"6", pid:"3"},
-          {name:"221", id:"7", pid:"4"},
-          {name:"222", id:"8", pid:"4"}
-        ]
+        this.serviceOriginData = JSON.parse(JSON.stringify(this.serviceData))
+      },
+      // 获取接口名称-ID-关联服务ID
+      getApiData: function() {
+        this.apiData = {
+          select: { name: "---", id: "0", pid: '0' },
+          items: [
+            { name: "---", id: "0", pid: '0' },
+            { name: "项目1-服务1-接口1", id: "1", pid: "1" },
+            { name: "项目1-服务1-接口2", id: "2", pid: "1" },
+            { name: "项目1-服务2-接口1", id: "3", pid: "2" },
+            { name: "项目1-服务2-接口2", id: "4", pid: "2" },
+            { name: "项目2-服务1-接口1", id: "5", pid: "3" },
+            { name: "项目2-服务1-接口2", id: "6", pid: "3" },
+            { name: "项目2-服务2-接口1", id: "7", pid: "4" },
+            { name: "项目2-服务2-接口2", id: "8", pid: "4" }
+          ]
+        },
+        this.apiOriginData = JSON.parse(JSON.stringify(this.apiData))
+      },
+      initMenu: function() {
+        // 首先拿到项目列表
+        let projectList = this.projectOriginData.items
+        // 拿到列表中的第一个项目(空项目)
+        this.projectSelected = this.projectData.select
+
+        // 使用空项目的id对服务的列表进行过滤,拿到一个空服务列表
+        let project = this.projectSelected
+        let serviceList = this.serviceOriginData.items.filter(function (item) { return item.pid == project.id })
+        // 将空服务列表插入到serviceData当中
+        this.serviceData.items = serviceList
+        // 从serviceData当中拿到第一个空的服务(初始化服务列表)
+        this.serviceSelected = this.serviceData.select
+
+        // 使用空服务的id对接口列表进行过滤,拿到一个空的接口列表
+        let service = this.serviceSelected
+        let apiList = this.apiOriginData.items.filter(function (item) { return item.pid == service.id })
+        // 将空接口列表插入到apiData当中
+        this.apiData.items = apiList
+        // 从apiData当中拿到第一个空的接口(初始化接口列表)
+        this.apiSelected = this.apiData.select
       }
     }
   }
